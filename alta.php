f1b8bf6ff982faaaac091a61ea939482ec0e215e
Fidel GB <fgarcia14@ucol.mx>
2018-11-26 12:37:30 -0600

    Solucionado error que impedia agregar nuevas salas #2

--- alta.php
+++ alta.php
@@ -11,19 +11,21 @@ if (isset($_GET["nom"])) {
     $ip = $_GET["ip"];
     $isdn = $_GET["isdn"];
     
-    
+    echo $isdn!=NULL;
     
     // Hay campos en blanco
-    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL|$isdn==NULL) {
-        echo "un campo est&aacute; vacio.";
-        formRegistro();
-    }else{
-
-                $query = 'INSERT INTO SALA_REMOTA (nombre, telefono, email_responsable, ip, issdn) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'")';
-                mysql_query($query) or die(mysql_error());
-                echo 'La sala '.$name.' ha sido registrada de manera satisfactoria.<br/>';
-   header('Location: index.php');
-            }
+    if($name!=NULL||$responsable!=NULL||$telefono!=NULL||$email!=NULL||$ip!=NULL||$isdn!=NULL) {
+        
+        $sql = "INSERT INTO SALA_REMOTA(nombre, responsable, telefono, email_responsable, ip, isdn) 
+                VALUES ('$name','$responsable','$telefono','$email','$ip','$isdn')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
-
+        
+    }
+    header("Location: listar.php");
+}
 ?>
