80012b5c5d51be98d10c78b0c5d8a2da90fd10f2
Fidel GB <fgarcia14@ucol.mx>
2018-11-26 13:19:29 -0600

    Errores de chrome solucionados 2.0 #4

--- editarSala.php
+++ editarSala.php
@@ -43,8 +43,6 @@ if (isset($_GET["id"])) {
     <!-- MetisMenu CSS -->
     <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
     <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -82,25 +80,18 @@ if (isset($_GET["id"])) {
     <input name= "ip" type="text" class="form-control" id="juan Perez"
            placeholder="Introduce IP"value= "<?=$ip;?>">
   </div>
-    <div class="form-group">
-    <label for="ejemplo_email_1">ISDN</label>
-    <input name= "isdn" type="text" class="form-control" id="juan Perez"
-           placeholder="Introduce ISDN" value= "<?=$isdn;?>">
-  </div>
+
   <button type="submit" class="btn btn-success">Enviar</button>
 </form>
 </div>
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="./js/jquery-1.11.0.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src="js/sb-admin-2.js"></script>
+
 
 </body>
