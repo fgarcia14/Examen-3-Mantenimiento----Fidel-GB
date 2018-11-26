0078b5b84b77c2a9cada265f09b4e0d6b208337c
Fidel GB <fgarcia14@ucol.mx>
2018-11-26 13:11:29 -0600

    Errores de chrome solucionados #4

--- listar.php
+++ listar.php
@@ -18,7 +18,7 @@
     <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/sb-admin-2.css" rel="stylesheet">
+    
  
 
 </head>
@@ -53,15 +53,10 @@
    <button type="button" class="btn btn-success" onclick="window.location.href='sala_remota.html'">Agregar Sala</button>
    </div>
     <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
+    <script src="./js/jquery-1.11.0.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../js/plugins/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../.js/sb-admin-2.js"></script>
 
 </body>
